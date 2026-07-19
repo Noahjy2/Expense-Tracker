92c6e6089281c1ed002f15437bda594ebe617552
David Heng <hengjinyongd@gmail.com>
2026-07-19 09:28:37 +0000

    Initialize Expense class

--- Expense.java
+++ Expense.java
@@ -1,3 +1,43 @@
 public class Expense {
-    
+    private int id;
+    private String category;
+    private double amount;
+    private String date;
+    private String description;
+
+
+    //Getter
+    public int getId(){
+        return this.id;
+    }
+    public String getCategory(){
+        return this.category;
+    }
+    public double getAmount(){
+        return this.amount;
+    }
+    public String getDate(){
+        return this.date;
+    }
+    public String getDescription(){
+        return this.description;
+    }
+
+
+    //Setter
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setCategory(String category){
+        this.category = category;
+    }
+    public void setAmount(double amount){
+        this.amount = amount;
+    }
+    public void setDate(String date){
+        this.date = date;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
 }
